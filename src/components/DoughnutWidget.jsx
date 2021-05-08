c610c7656b0f903fb1db1ae17d5198251e008cf5
David Riedl <daves.weblab@gmail.com>
2021-05-08 21:54:51 +0200

    feat(doughnut): completely written doughnut widget with highcharts.

--- src/components/DoughnutWidget.jsx
+++ src/components/DoughnutWidget.jsx
@@ -1,19 +1,52 @@
-import {useState, useRef} from "react";
+import {useState, useRef, useEffect} from "react";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
+import {colorToRgba} from "theme/utils/color";
 
 const AlignCenter = mobro.hooks.getComponent("shared.layout.align-center");
 const LoadingIndicator = mobro.hooks.getComponent("shared.loading-indicator");
 
-function load() {
-    return function() {
+const defaultFrontColor = "rgb(0 255 255)";
+const defaultBackColor = "rgba(100, 100, 100, 0.5)";
+const defaultFontColor = "white";
+
+function basicTextFontColor(configRef, layoutConfigRef) {
+    return colorToRgba(configRef.current.widgetFontColor, colorToRgba(layoutConfigRef.current.widgetFontColor, defaultFontColor));
+}
+
+function frontColor(configRef) {
+    return colorToRgba(configRef.current.frontColor, defaultFrontColor);
+}
+
+function backColor(configRef) {
+    return colorToRgba(configRef.current.backColor, defaultBackColor);
+}
+
+function maxValue(configRef, channelData) {
+    if(configRef.current.maxValue) {
+        return parseInt(configRef.current.maxValue)
+    }
+
+    if(mobro.utils.channelData.isPercentageData(channelData.current)) {
+        return 100;
+    }
+
+    if(channelData.current) {
+        return mobro.utils.channelData.extractRawMaxValue(channelData.current)
+    }
+
+    return 100;
+}
+
+function load(configRef, layoutConfigRef) {
+    return function () {
         this.widgetLabel = this.renderer.text("")
             .attr({
                 align: "center",
                 zIndex: 1
             })
             .css({
-                color: "white"
+                color: basicTextFontColor(configRef, layoutConfigRef)
             })
             .add()
 
@@ -23,7 +56,7 @@ function load() {
                 zIndex: 2
             })
             .css({
-                color: "red"
+                color: frontColor(configRef)
             })
             .add();
 
@@ -33,14 +66,14 @@ function load() {
                 zIndex: 1
             })
             .css({
-                color: "white"
+                color: basicTextFontColor(configRef, layoutConfigRef)
             })
             .add()
     }
 }
 
-function redraw(config, channelData) {
-    return function() {
+function redraw(configRef, layoutConfigRef, channelData) {
+    return function () {
         if (!channelData.current) {
             return;
         }
@@ -52,11 +85,12 @@ function redraw(config, channelData) {
 
         this.widgetLabel
             .attr({
-                text: config.label ?? channelData.current?.label,
+                text: configRef.current.label ?? channelData.current?.label,
                 x: centerX,
                 y: centerY - (this.plotHeight / 6)
             })
             .css({
+                color: basicTextFontColor(configRef, layoutConfigRef),
                 fontSize: `${labelFontSize}px`
             });
 
@@ -67,6 +101,7 @@ function redraw(config, channelData) {
                 y: centerY + (valueFontSize / 3)
             })
             .css({
+                color: frontColor(configRef),
                 fontSize: `${valueFontSize}px`
             });
 
@@ -77,28 +112,25 @@ function redraw(config, channelData) {
                 y: centerY + (valueFontSize / 3) + (this.plotHeight / 6)
             })
             .css({
+                color: basicTextFontColor(configRef, layoutConfigRef),
                 fontSize: `${labelFontSize}px`
             });
     }
 }
 
-function DoughnutWidget(props) {
-    const {
-        config,
-        layoutConfig
-    } = props;
-
-    const channelData = useRef(null);
-    const [options, setOptions] = useState({
+function createOptions(config, layoutConfig, channelData) {
+    return {
+        colors: [frontColor(config)],
         chart: {
             type: "column",
             inverted: true,
             polar: true,
             backgroundColor: "rgba(0, 0, 0, 0)",
-            margin: 0,
+            margin: [0, 0, 0, 0],
+            spacing: [0, 0, 0, 0],
             events: {
-                load: load(),
-                redraw: redraw(config, channelData)
+                load: load(config, layoutConfig),
+                redraw: redraw(config, layoutConfig, channelData)
             }
         },
         title: {
@@ -115,10 +147,12 @@ function DoughnutWidget(props) {
         },
         pane: {
             center: ["50%", "50%"],
+            size: "100%",
             background: {
                 outerRadius: "100%",
                 innerRadius: "76%",
-                borderWidth: 0
+                borderWidth: 0,
+                backgroundColor: backColor(config)
             }
         },
         legend: {
@@ -134,8 +168,7 @@ function DoughnutWidget(props) {
             visible: false
         },
         yAxis: {
-            // todo change based on settings
-            max: 100,
+            max: maxValue(config, channelData),
             // no borders, ticks what so ever
             visible: false
         },
@@ -145,6 +178,9 @@ function DoughnutWidget(props) {
                 // due to unfinished animations
                 animation: false
             },
+            bar: {
+                groupPadding: 0
+            },
             column: {
                 // remove border from bar
                 borderWidth: 0
@@ -153,16 +189,39 @@ function DoughnutWidget(props) {
         series: [{
             data: [0]
         }]
-    });
+    }
+}
+
+function DoughnutWidget(props) {
+    const {
+        config,
+        layoutConfig
+    } = props;
+
+    const channelData = useRef(null);
+    const configRef = useRef(config);
+    const layoutConfigRef = useRef(config);
+    const [options, setOptions] = useState(createOptions(configRef, layoutConfigRef, channelData));
+    const optionsRef = useRef(options);
+
+    useEffect(() => {
+        configRef.current = config;
+        optionsRef.current.yAxis.max = maxValue(configRef, channelData);
+        optionsRef.current.pane.background.backgroundColor = backColor(configRef);
+        optionsRef.current.colors = [frontColor(configRef)];
+
+        setOptions({...optionsRef.current});
+    }, [config?.maxValue, config?.widgetFontColor, config?.frontColor, config?.backColor])
+
+    useEffect(() => {
+        layoutConfigRef.current = layoutConfig;
+    }, [])
 
     mobro.utils.component.useChannelListener(config?.channel, (data) => {
         channelData.current = data;
-        setOptions({
-            ...options,
-            series: [{
-                data: [parseFloat(mobro.utils.channelData.extractValue(data))]
-            }]
-        })
+        optionsRef.current.series[0].data = [parseFloat(mobro.utils.channelData.extractValue(data))];
+
+        setOptions({...optionsRef.current});
     });
 
     return (
